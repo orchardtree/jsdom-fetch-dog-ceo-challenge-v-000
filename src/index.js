ebebf2601137cfe678d4eb4c3bc8ceb9b77b4e45
Jesse Wenger <wengerj@gmail.com>
2021-06-06 05:05:55 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -59,7 +59,7 @@ function breedListener() {
 
 function filterListener() {
   document.querySelector("#breed-dropdown").addEventListener("change",function(event) {
-  const dropdownSelection = document.querySelector("#breed-dropdown").value
+    const dropdownSelection = document.querySelector("#breed-dropdown").value
     const breedList = document.getElementsByClassName("breed");
     for (const breed of breedList) {
       if (breed.innerHTML[0] != dropdownSelection) {

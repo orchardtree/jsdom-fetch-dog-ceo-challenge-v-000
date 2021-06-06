b5d065a1bb16ab3a5ad6d9501c9f20707cf024d8
Jesse Wenger <wengerj@gmail.com>
2021-06-06 03:37:52 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -67,10 +67,9 @@ function filterListener() {
 
 function myFunction() {
     var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById("myInput");
-    //filter = input.value.toUpperCase();
-    filter = input.value;
-    ul = document.getElementById("myUL");
+    input = document.getElementById("breed-dropdown");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("dog-breeds");
     li = ul.getElementsByTagName("li");
     for (i = 0; i < li.length; i++) {
         a = li[i].getElementsByTagName("a")[0];

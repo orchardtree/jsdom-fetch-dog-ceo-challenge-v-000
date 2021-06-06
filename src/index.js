0042174d89acf2bc4ac278793d2328ceeae3a941
Jesse Wenger <wengerj@gmail.com>
2021-06-06 04:41:56 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -67,6 +67,9 @@ function filterListener() {
 
 function myFunction() {
   breedList = document.getElementsByClassName("breed");
+  for (const breed of breedList) {
+    console.log(breed)
+    }
   }
 
 document.addEventListener('DOMContentLoaded', function() {

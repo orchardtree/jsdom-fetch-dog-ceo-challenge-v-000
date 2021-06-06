21cfb4168ff6aca4d28740fc2f8c9e0c7cdc7429
Jesse Wenger <wengerj@gmail.com>
2021-06-06 05:01:04 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -67,7 +67,7 @@ function filterListener() {
   for (const breed of breedList) {
     if (breed.innerHTML[0] != dropdownSelection) {
       //console.log(breed.innerHTML)
-      breed.style.display = "none";
+      //breed.style.display = "none";
       }
     };
   }
